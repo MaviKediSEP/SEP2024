d1bdedf0917f3a94db355b394df3273cce3cbc5c
JohannesMoseler <joyo99@gmx.de>
2024-04-19 18:16:24 +0200

    Inhalt der neu generierten Datein auf den ApacheServer pushen

--- CodyUpdateCheck.js
+++ CodyUpdateCheck.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const fs = require('fs');
+let addedFiles = [];
 
 // Get the absolute path to the cody-engine-main/packages/be directory
 const myFolder = path.join(__dirname, '/cody-engine-main/packages/be');
@@ -17,7 +18,7 @@ fs.readdir(myFolder, (err, oldDir) => {
         return;
       }
       // Check for changes in the directory contents
-      const addedFiles = getAddedFiles(newDir, oldDir);
+      addedFiles = findAddedFiles(newDir, oldDir);
       if (addedFiles.length > 0) {
         console.log('New files found:');
         addedFiles.forEach(file => {
@@ -32,13 +33,21 @@ fs.readdir(myFolder, (err, oldDir) => {
           });
         });
         oldDir = newDir; // Update oldDir to reflect the current directory state
-      } else {
-        console.log('No new files');
       }
     });
   }, 5000);
 });
 
-function getAddedFiles(newDir, oldDir) {
+function findAddedFiles(newDir, oldDir) {
   return newDir.filter(file => !oldDir.includes(file));
 }
+function getAddedFiles(){
+    return addedFiles
+};
+function getMyFolder(){
+    return myFolder;
+};
+module.exports = {
+    getAddedFiles,
+    getMyFolder
+};
