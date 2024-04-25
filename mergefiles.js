86d16592bfe71a62f26392f8be3dd9c7dc7f6e0e
Crazyrx7 <124936672+Crazyrx7@users.noreply.github.com>
2024-04-25 15:54:39 +0200

    mergefiles.js hinzugefügt
    script hinzugefügt um mehrere .txt files zusammen zu führen

---
+++ mergefiles.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const path = require('path');
+
+// Function to scan a directory and get all .txt files
+function getAllTxtFiles(directoryPath, callback) {
+    fs.readdir(directoryPath, function (err, files) {
+        if (err) {
+            return callback(err);
+        }
+        
+        const txtFiles = files.filter(file => path.extname(file) === '.txt');
+        callback(null, txtFiles);
+    });
+}
+
+// Function to concatenate contents of all .txt files into one
+function concatenateTxtFiles(txtFiles, outputPath) {
+    const outputStream = fs.createWriteStream(outputPath);
+
+    txtFiles.forEach(txtFile => {
+        const filePath = path.join(__dirname, txtFile);
+        const fileContents = fs.readFileSync(filePath, 'utf8');
+        outputStream.write(fileContents);
+        outputStream.write('\n');
+    });
+
+    outputStream.end();
+}
+
+const folderPath = __dirname; // Change this to your folder path
+const outputPath = './output.txt'; // Change this to your desired output file path
+
+getAllTxtFiles(folderPath, function(err, txtFiles) {
+    if (err) {
+        console.error('Error reading directory:', err);
+        return;
+    }
+    
+    if (txtFiles.length === 0) {
+        console.log('No .txt files found in the directory.');
+        return;
+    }
+
+    console.log('Concatenating the following .txt files:', txtFiles);
+    concatenateTxtFiles(txtFiles, outputPath);
+    console.log('Concatenation completed. Check output.txt.');
+});
